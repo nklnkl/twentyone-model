11a3a3f91d5ef59599ad12e69d9c9ac82aaf31a0
niko lagman <lagman.niko@gmail.com>
2018-10-18 04:00:10 -0400

    Add Test flow test

---
+++ test/example.test.ts
@@ -0,0 +1,8 @@
+import { describe, it } from 'mocha';
+import { assert } from 'chai';
+
+describe('Test Framework', () => {
+  it('should pass if testing flow is valid', () => {
+    assert.ok(true);
+  });
+});
