2de57a48ca9d8c79a7d58e8932c72a53c87c52fb
niko lagman <lagman.niko@gmail.com>
2018-10-19 03:46:13 -0400

    Add Card class

--- src/card.ts
+++ src/card.ts
@@ -1,7 +1,21 @@
-export { ICard };
+export { ICard, Card };
 
 interface ICard {
   index: number;
   suit: number;
   value: number;
+}
+
+class Card implements ICard {
+  
+  public index: number;
+  public suit: number;
+  public value: number;
+
+  constructor (index: number, suit: number, value: number) {
+    this.index = index;
+    this.suit = suit;
+    this.value = value;
+  }
+  
 }
